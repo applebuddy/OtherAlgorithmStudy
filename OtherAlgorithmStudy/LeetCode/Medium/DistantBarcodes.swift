dc7b27f4c0c2d4b44d8f76c22e094a3059148d0f
MinKyeongTae <kyeongtea92@gmail.com>
2020-01-18 04:54:54 +0900

    LeetCode/Medium/DistantBarcodes Swift 문제풀이

---
+++ OtherAlgorithmStudy/LeetCode/Medium/DistantBarcodes.swift
@@ -0,0 +1,66 @@
+//
+//  DistantBarcodes.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/01/18.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: DistantBarcodes Swift 문제풀이
+
+/*
+ import Foundation
+
+ class Solution {
+ func rearrangeBarcodes(_ barcodes: [Int]) -> [Int] {
+     var dic = [Int:Int]()
+     var pv = [(Int,Int)]()
+     var Ans = [Int]()
+     if barcodes.count <= 2 { return barcodes }
+     for i in barcodes.indices {
+         dic[barcodes[i]] = (dic[barcodes[i]] ?? 0) + 1
+     }
+
+     for (key, value) in dic {
+         pv.append((key, value))
+     }
+
+     pv.sort {
+         $0.1 < $1.1 || ($0.1 == $1.1 && $0.0 < $1.0)
+     }
+
+     for i in barcodes.indices {
+         if i == 0 {
+             Ans.append(pv[pv.count-1].0)
+             pv[pv.count-1].1 -= 1
+         } else {
+             if pv.last!.0 == Ans.last! {
+                 if pv.count == 1 {
+                     var idx = 0
+                     for _ in 0..<pv[0].1 {
+                         Ans.insert(pv[0].0, at: idx)
+                         idx += 2
+                     }
+                     break
+                 }
+                 Ans.append(pv[pv.count-2].0)
+                 pv[pv.count-2].1 -= 1
+                 if pv[pv.count-2].1 <= 0 {
+                     pv.remove(at: pv.count-2)
+                 }
+             } else {
+                 Ans.append(pv[pv.count-1].0)
+                 pv[pv.count-1].1 -= 1
+                 if pv[pv.count-1].1 <= 0 {
+                     pv.remove(at: pv.count-1)
+                 }
+             }
+         }
+
+         if pv.isEmpty { break }
+     }
+
+     return Ans
+ }
+ }
+ */
--- OtherAlgorithmStudy/LeetCode/Medium/InsertDeleteGetRandomO(1).swift
+++ OtherAlgorithmStudy/LeetCode/Medium/InsertDeleteGetRandomO(1).swift
@@ -9,21 +9,21 @@
 // MARK: InsertDeleteGetRandomO(1) Swift 풀이답안
 
 /*
-import Foundation
- 
-class RandomizedSet {
+ import Foundation
+
+ class RandomizedSet {
 
     private var dic = [Int:Int]()
     private var arr = [Int]()
-    
-    /** Initialize your data structure here. */
+
+ /** Initialize your data structure here. */
     init() {
-        
+
     }
-    
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+
+ /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     func insert(_ val: Int) -> Bool {
-        
+
         if let index = dic.index(forKey: val) { return false }
         else {
             arr.append(val)
@@ -32,10 +32,10 @@ class RandomizedSet {
             return true
         }
     }
-    
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
+
+ /** Removes a value from the set. Returns true if the set contained the specified element. */
     func remove(_ val: Int) -> Bool {
-      
+
         if let index = dic.index(forKey: val) {
             let lastIndex = arr.count - 1
             let valIndex = dic[val]!
@@ -49,19 +49,19 @@ class RandomizedSet {
         }
         else { return false }
     }
-    
-    /** Get a random element from the set. */
+
+ /** Get a random element from the set. */
     func getRandom() -> Int {
         let randVal = Int.random(in: 0..<arr.count)
         return arr[randVal]
     }
-}
+ }
 
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * let obj = RandomizedSet()
- * let ret_1: Bool = obj.insert(val)
- * let ret_2: Bool = obj.remove(val)
- * let ret_3: Int = obj.getRandom()
+ /**
+   * Your RandomizedSet object will be instantiated and called as such:
+   * let obj = RandomizedSet()
+   * let ret_1: Bool = obj.insert(val)
+   * let ret_2: Bool = obj.remove(val)
+   * let ret_3: Int = obj.getRandom()
+  */
  */
-*/
