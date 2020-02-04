c9fa0f20cbde8324c289908a5ebfd0cda7385fa9
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-05 01:03:39 +0900

    DeleteANode SinglyLinkedList Swift 문제풀이

---
+++ OtherAlgorithmStudy/Hackerrank/DeleteANode.swift
@@ -0,0 +1,30 @@
+//
+//  DeleteANode.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/05.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: Delete a Node
+/*
+ 
+import Foundation
+ 
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     data: Int
+ *     next: SinglyLinkedListNode?
+ * }
+ *
+ */
+ 
+func deleteNode(llist: SinglyLinkedListNode?, position: Int) -> SinglyLinkedListNode? {
+    if position == 0 { return llist?.next }
+    llist?.next = deleteNode(llist: llist?.next, position: position - 1)
+    return llist
+}
+ 
+*/
