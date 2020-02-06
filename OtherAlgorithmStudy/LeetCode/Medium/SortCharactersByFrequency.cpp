e0d2d561a830e66910927dc2af11cd95dc6f004f
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-06 16:03:25 +0900

    LeetCode 일부문제(2문제) 주석설명 추가

--- OtherAlgorithmStudy/LeetCode/Medium/SortCharactersByFrequency.cpp
+++ OtherAlgorithmStudy/LeetCode/Medium/SortCharactersByFrequency.cpp
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Min Kyeong Tae. All rights reserved.
 //
 
-// MARK: SortCharacterByFrequency 문제풀이
+// MARK: Sort-Character-By-Frequency 문제풀이 + 주석추가
 
 #if 0
 #include <string>
@@ -22,12 +22,18 @@ public:
         map<char,int> MP;
         vector<Pair> PV;
         string Ans = "";
+        // 시퀀스 요소의 빈도수를 MP에 기록
         for(int i=0; i<s.length(); i++) MP[s[i]]++;
+        
+        // MP의 빈도수 기록을 PV vector에 기록
         for(auto &mp : MP) PV.push_back({mp.first, mp.second});
+        
+        // 빈도수가 높은순으로, 만약 빈도수가 동일하다면 사전순으로 정렬한다.
         sort(PV.begin(), PV.end(), [](const Pair &a, const Pair &b) {
             return a.second > b.second || (a.second == b.second && a.first < b.first);
         });
         
+        // 정렬된 문자의 값을 문자열 변수 Ans에 추가 후 정답을 반환한다.
         for(auto v: PV) Ans += string(v.second,v.first);
         return Ans;
     }
--- OtherAlgorithmStudy/LeetCode/Medium/SplitArrayIntoConsecutiveSubsequences.cpp
+++ OtherAlgorithmStudy/LeetCode/Medium/SplitArrayIntoConsecutiveSubsequences.cpp
@@ -8,29 +8,48 @@
 
 // MARK: SplitArrayIntoConsecutiveSubsequences
 
-// MARK: - map 사용 좀더 이해쉽지만 좀더 긴 문제풀이
+// MARK: - map 사용 좀더 이해쉽지만 좀더 긴 문제풀이 + 주석추가
 
 #if 0
 #include <iostream>
 #include <map>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
     bool isPossible(vector<int>& nums) {
+        // * MP: 배열 요소들의 빈도수를 기록할 맵 자료구조
+        // * CMP: 3연속 시퀀스 이후 있을 수 있는 값을 키로 기록하는 맵 자료구조
+        // ex) 1,2,3 세번 연속이 있을경우, 4에 대해 CMP 키값으로 기록
         map<int,int> MP, CMP;
+        
+        // 배열 요소 빈도수를 MP에 기록
         for(auto &v: nums) MP[v]++;
+        
+        // 배열을 순회하며 모든 요소가 최소 3회이상의 연속값으로 이루어져있는지 확인한다.
         for(auto &v: nums) {
+            // 해당 위치 값이 연속값으로 사용되었으면 건너뛴다
             if(MP[v]==0) continue;
+            
+            // 해당 위치값이 앞선 3회연속 시퀀스 이후의 값이면 현 위치값+1을 CMP로 재 기록하고
+            // 현재 위치 값은 1 감소 시킨다.
             if(CMP[v]>0) {
                 CMP[v]--;
                 CMP[v+1]++;
                 MP[v]--;
+                
+                // CMP[v]==0 이라는것은 아직 해당 값이 최소3회의 연속값에 포함되지 않았음을 의미
+                // 현재 값에서 최소 3번의 연속값은 존재해야하므로 이를 체크한다.
+                // * CMP의 값은 이미 3연속 시퀀스를 충족한 값들이 들어간다.
             } else if (CMP[v]==0) {
                 int now = v, next = v+1, nnext = v+2;
+                // 만약 3연속의 시퀀스값이 존재하지 않으면 false를 반환한다.
                 if(MP[now]==0 || MP[next]==0 || MP[nnext]==0) {
                     return false;
                 }
+                
+                // 3연속 시퀀스 값이 존재하면 3연속 시퀀스 값을 MP에서 1씩 감소시키고, 3연속 시퀀스 + 1의 값을 CMP에 기록한다.
                 MP[now]--;
                 MP[next]--;
                 MP[nnext]--;
@@ -38,6 +57,7 @@ public:
             }
         }
         
+        // nums 순회를 마치게 된것은 최소 3연속 시퀀스가 성립했음을 의미하므로 true를 반환한다.
         return true;
     }
 };
