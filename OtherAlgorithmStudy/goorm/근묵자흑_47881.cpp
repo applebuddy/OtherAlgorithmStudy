04e79770411ea447963634d6f2e62b92b6fd2b73
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-21 19:54:03 +0900

    근묵자흑_47881 Math C++ 문제풀이

---
+++ OtherAlgorithmStudy/goorm/근묵자흑_47881.cpp
@@ -0,0 +1,21 @@
+//
+//  근묵자흑_47881.cpp
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/21.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 근묵자흑 47881
+// MARK: Math 문제풀이
+#if 0
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N,K; cin>>N>>K;
+    printf("%d\n",(N-1-1)/(K-1)+1);
+    return 0;
+}
+#endif
