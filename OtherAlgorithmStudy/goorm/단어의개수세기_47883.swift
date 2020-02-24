6f6b5d03732e90ad32935194e5b572a166131153
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-25 04:23:54 +0900

    단어의개수세기_47883 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/단어의개수세기_47883.swift
@@ -0,0 +1,16 @@
+//
+//  단어의개수세기_47883.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/25.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 단어의 개수 세기 47883
+// MARK: 문제풀이
+
+/*
+import Foundation
+let arr = readLine()!.split(separator: " ")
+print(arr.count)
+*/
