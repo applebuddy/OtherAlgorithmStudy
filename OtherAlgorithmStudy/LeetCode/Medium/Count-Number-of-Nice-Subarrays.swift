b16d7a1beae2d2ace65f7e816f8946c336cc744d
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-24 16:48:36 +0900

    Count-Number-of-Nice-Subarrays swift 문제풀이

---
+++ OtherAlgorithmStudy/LeetCode/Medium/Count-Number-of-Nice-Subarrays.swift
@@ -0,0 +1,28 @@
+//
+//  Count-Number-of-Nice-Subarrays.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/24.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - Count-Number-of-Nice-Subarrays
+/*
+import Foundation
+
+class Solution {
+    func numberOfSubarrays(_ nums: [Int], _ k: Int) -> Int {
+        var DP = [Int](repeating: 0, count: nums.count+1)
+        DP[0] = 1
+        var idx = 0, Ans = 0
+        for i in nums.indices {
+            idx += nums[i] & 1
+            DP[idx] += 1
+            if idx >= k {
+                Ans += DP[idx-k]
+            }
+        }
+        return Ans
+    }
+}
+*/
