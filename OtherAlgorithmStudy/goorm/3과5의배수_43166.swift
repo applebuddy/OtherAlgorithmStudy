1c13b60f0783696048f53834797b1ae352fdeab0
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-24 18:14:56 +0900

    3과5의배수_43166 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/3과5의배수_43166.swift
@@ -0,0 +1,23 @@
+//
+//  3과5의배수_43166.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/24.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 3과5의배수 43166
+// MARK: Math 문제풀이
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+
+if N <= 2 { print(0); exit(0) }
+var Ans = 0
+for i in 3...N {
+        if i % 3 == 0 || i % 5 == 0 { Ans += i }
+}
+
+print(Ans)
+*/
