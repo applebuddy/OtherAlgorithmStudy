d040e4232311f9172db771f2b1a106d2a7ee4dc8
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-24 18:22:16 +0900

    리그경기횟수구하기_43092 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/리그경기횟수구하기_43092.swift
@@ -0,0 +1,21 @@
+//
+//  리그경기횟수구하기_43092.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/24.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 리그경기횟수구하기 43092
+// MARK: 문제풀이
+
+/*
+import Foundation
+let N = Int(readLine()!)!
+var Ans = 0
+for i in 1..<N {
+    Ans += i
+}
+
+print(Ans)
+*/
