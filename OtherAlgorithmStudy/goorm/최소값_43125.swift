d979fa8ab0737301dad1aafb9a4fd79f3c8378c0
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-24 20:12:15 +0900

    최소값_43125 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/최소값_43125.swift
@@ -0,0 +1,18 @@
+//
+//  최소값_43125.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/24.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 최소값 43125
+// MARK: 문제풀이
+
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+print(arr.min()!)
+*/
