3b350ee7d2598adfaa2717262b0c118318a1ddf1
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-25 03:26:54 +0900

    소수판별_43238 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/소수판별_43238.swift
@@ -0,0 +1,23 @@
+//
+//  소수판별_43238.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/25.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 소수판별_43238
+// MARK: O(sqrt(N)) TimeComplexity 문제풀이
+
+/*
+import Foundation
+let N = Int(readLine()!)!
+var i = 2
+var flag = true
+while i * i < N {
+        if N % i == 0 { flag = false; break }
+        i += 1
+}
+
+print(flag ? "True" : "False")
+*/
