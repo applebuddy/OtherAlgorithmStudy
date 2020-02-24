8ac38e40b336ac7ff208c6eb513d29a699e740b6
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-25 04:28:41 +0900

    네수의곱_43112 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/네수의곱_43112.swift
@@ -0,0 +1,20 @@
+//
+//  네수의곱_43112.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/25.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 네수의곱_43112
+// MARK: 문제풀이
+
+/*
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+
+func mul(_ A: Int, _ B: Int) -> Int {
+        return A * B
+}
+
+print(mul(mul(arr[0],arr[1]),mul(arr[2],arr[3])))
+*/
