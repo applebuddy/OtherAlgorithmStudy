f2e74bd3352c8590470842ef0e9ead67886a07d8
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-24 18:19:04 +0900

    홀짝판별_43111 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/홀짝판별_43111.swift
@@ -0,0 +1,16 @@
+//
+//  홀짝판별_43111.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/24.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 홀짝판별 43111
+// MARK: 문제풀이
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+print(N & 1 == 1 ? "odd" : "even")
+*/
