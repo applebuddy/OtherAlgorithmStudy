23eb73cafb0c66b4c7f0b1462ec7d7c371a8cae2
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-25 03:20:54 +0900

    약수의합_43232 C++ 문제풀이

---
+++ OtherAlgorithmStudy/goorm/약수의합_43232.swift
@@ -0,0 +1,19 @@
+//
+//  약수의합_43232.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/25.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 약수의합 43232
+// MARK: 문제풀이
+/*
+let N = Int(readLine()!)!
+var Ans = 0
+for i in 1...N {
+        if N % i == 0 { Ans += i }
+}
+
+print(Ans)
+*/
