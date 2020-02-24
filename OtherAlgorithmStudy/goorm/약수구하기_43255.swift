b19669e9f91c347c6b76dc2a83c428301d446c50
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-24 18:09:39 +0900

    약수구하기_43255 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/약수구하기_43255.swift
@@ -0,0 +1,18 @@
+//
+//  약수구하기_43255.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/24.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: 약수구하기 문제풀이
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+
+for i in 1...N {
+        if N % i == 0 { print(i, terminator: " ") }
+}
+*/
