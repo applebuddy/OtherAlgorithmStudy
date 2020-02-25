bca1f023482b7ec20a68bfdfff9a3adf90a34591
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-25 20:21:46 +0900

    배열합치기_43250 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/배열합치기_43250.swift
@@ -0,0 +1,21 @@
+//
+//  배열합치기_43250.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/25.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 배열 합치기 43250
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+let M = arr[0], N = arr[1]
+var arr2 = readLine()!.split(separator: " ").map { Int($0)! }
+let arr3 = readLine()!.split(separator: " ").map { Int($0)! }
+arr2.append(contentsOf: arr3[arr3.indices])
+arr2.sorted(by: <).forEach { print($0, terminator: " ") }
+*/
