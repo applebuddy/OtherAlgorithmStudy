8bee2d41f158fb3e62e47a005e9af27f0f020511
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-25 19:18:43 +0900

    완전제곱수_43152 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/완전제곱수_43152.swift
@@ -0,0 +1,27 @@
+//
+//  완전제곱수_43152.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/25.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 완전제곱수 43152
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+
+let N = Int(readLine()!)!
+var Ans = 0
+
+for _ in 0..<N {
+        let num = Int(readLine()!)!
+        let square = sqrt(Double(num))
+      if Double(Int(square)) == square {
+                Ans += 1
+        }
+}
+
+print(Ans)
+*/
