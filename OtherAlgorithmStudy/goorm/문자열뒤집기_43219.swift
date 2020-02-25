9a2fd71bc8cef9e92a66fe3de5c793f613697f94
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-25 19:39:42 +0900

    문자열뒤집기_43219 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/문자열뒤집기_43219.swift
@@ -0,0 +1,16 @@
+//
+//  문자열뒤집기_43219.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/25.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 문자열뒤집기 43219
+// MARK: swift 문제풀이
+
+/*
+import Foundation
+let S = Array(readLine()!).map { String($0) }.reversed().joined()
+print(S)
+*/
