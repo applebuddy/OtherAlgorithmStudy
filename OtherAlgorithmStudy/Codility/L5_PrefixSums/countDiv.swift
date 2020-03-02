f3e05d1455ba38d44791441a3d8374a390346ea5
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-03 03:29:15 +0900

    Codility/Lesson5/CountDiv swift 문제풀이

---
+++ OtherAlgorithmStudy/Codility/L5_PrefixSums/countDiv.swift
@@ -0,0 +1,22 @@
+//
+//  countDiv.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/03/03.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - CountDiv
+// MARK: swift 100% 문제풀이
+/// * A...B의 K로 나눠지는 수의 갯수를 반환하는 문제, O(1) 로 풀 수 있다.
+
+/*
+import Foundation
+
+public func solution(_ A : Int, _ B : Int, _ K : Int) -> Int {
+    let first = A == 0 ? 0 : ((A-1) / K + 1)
+    let last = (B / K)
+
+    return last - first + 1
+}
+*/
