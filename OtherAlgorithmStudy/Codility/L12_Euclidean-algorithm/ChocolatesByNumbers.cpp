06a776e692a2f33a8bab9eedacef2cd7e41e565b
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-06 13:53:02 +0900

    Codility/L12/Chocolates-by-numbers math C++ 문제풀이

---
+++ OtherAlgorithmStudy/Codility/L12_Euclidean-algorithm/ChocolatesByNumbers.cpp
@@ -0,0 +1,23 @@
+//
+//  ChocolatesByNumbers.cpp
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/03/06.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - Chocolates-by-numbers
+// MARK: math C++ 문제풀이
+
+#if 0
+#include <iostream>
+using namespace std;
+
+int getGCD(int A, int B) {
+    if(B==0) return A;
+    return getGCD(B, A%B);
+}
+int solution(int N, int M) {
+    return N/getGCD(N,M);
+}
+#endif
