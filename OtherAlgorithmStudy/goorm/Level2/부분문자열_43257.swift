861e2cee815cac57662b8226a20f829c5e99759a
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-10 21:22:33 +0900

    부분문자열_43257 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/Level2/부분문자열_43257.swift
@@ -0,0 +1,16 @@
+//
+//  부분문자열_43257.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/03/10.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 부분문자열
+/*
+let input = Array(readLine()!).map { String($0) }
+
+for i in input.indices {
+        print(input[0...i].joined())
+}
+*/
