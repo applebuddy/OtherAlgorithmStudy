0efb4b7e32fce4402db25e63ed35daf0294fcf68
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-11 01:16:45 +0900

    유클리드호제법_43091 swift 복습 문제풀이

---
+++ OtherAlgorithmStudy/goorm/Level2/유클리드호제법_43091.swift
@@ -0,0 +1,22 @@
+//
+//  유클리드호제법_43091.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/02/25.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 유클리드호제법 43091
+// MARK: swift 문제풀이
+
+/*
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+
+func getGCD(_ A: Int, _ B: Int) -> Int {
+        if B == 0 { return A }
+        return getGCD(B, A%B)
+}
+
+print(getGCD(input[0], input[1]))
+*/
+
