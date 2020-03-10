0e99cfe0a7b793a193ae16d28c3cd86e186f6bb2
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-11 01:13:05 +0900

    두점사이의거리_43228 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/Level2/두점사이의거리_43228.swift
@@ -0,0 +1,18 @@
+//
+//  두점사이의거리_43228.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/03/11.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 두점 사이의 거리 43228
+/*
+import Foundation
+
+let first = readLine()!._split(separator: " ").map { Double($0)! }
+let second = readLine()!._split(separator: " ").map { Double($0)! }
+let mul = (first[0]-second[0]) * (first[0]-second[0]) + (first[1]-second[1]) * (first[1]-second[1])
+var sqrtNum: Double = sqrt(mul) * 100.0
+print(sqrtNum.rounded()/100)
+*/
