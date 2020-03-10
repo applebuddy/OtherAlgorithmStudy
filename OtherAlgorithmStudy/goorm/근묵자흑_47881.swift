5f39bf7a0ec4b2b290ce2d51df53887a2a7ce3ad
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-11 01:00:23 +0900

    근묵자흑_47881 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/근묵자흑_47881.swift
@@ -0,0 +1,14 @@
+//
+//  근묵자흑_47881.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/03/11.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 근묵자흑 47881
+/*
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let N = input[0], K = input[1]
+print(((N-K-1)/(K-1))+2)
+*/
