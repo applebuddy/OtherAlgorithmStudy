fd9376dc96ce6e9f40b5ce14b7be1d7efc6bd928
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-11 00:47:58 +0900

    홀수의합_43124 swift 문제풀이

---
+++ OtherAlgorithmStudy/goorm/Level2/홀수의합_43124.swift
@@ -0,0 +1,14 @@
+//
+//  홀수의합_43124.swift
+//  OtherAlgorithmStudy
+//
+//  Created by MinKyeongTae on 2020/03/11.
+//  Copyright © 2020 Min Kyeong Tae. All rights reserved.
+//
+
+// MARK: - 홀수의 합
+/*
+let input = readLine()!._split(separator: " ").map { Int($0)! }
+let A = input[0], B = input[1]
+print([Int](A...B).filter { $0 & 1 == 1 }.reduce(0,+))
+*/
