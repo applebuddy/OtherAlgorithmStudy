9f871fd130ffb1ebe3564a939ad39aa8c556c9b9
MinKyeongTae <kyeongtea92@gmail.com>
2020-07-28 01:15:17 +0900

    Solved Single-Number using swift

---
+++ OtherAlgorithmStudy/LeetCode/30-Day-Challenge/Single-Number.swift
@@ -0,0 +1,30 @@
+
+// MARK: - TimeComplexity O(N), SpaceComplexity O(1) Solution
+/*
+func singleNumber(_ nums: [Int]) -> Int {
+    var result = 0
+    for num in nums {
+        result ^= num
+    }
+    return result
+}
+*/
+
+// MARK: - TimeComplexity O(N), SpaceComplexity O(N) Solution
+/*
+func singleNumber(_ nums: [Int]) -> Int {
+    var dic = [Int: Int]()
+    var dic2 = [Int: Int]()
+    var Ans = 0
+
+    for num in nums {
+        dic[num] = (dic[num] ?? 0) + 1
+    }
+
+    for d in dic {
+        if d.value == 1 { return d.key }
+    }
+
+    return 0
+}
+*/
