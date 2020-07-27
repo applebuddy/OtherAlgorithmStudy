101909e7551a5bf85a8db5c3046d8de787879a2b
MinKyeongTae <kyeongtea92@gmail.com>
2020-07-28 01:26:10 +0900

    Solved Happy-Number using swift

---
+++ OtherAlgorithmStudy/LeetCode/30-Day-Challenge/Happy-Number.swift
@@ -0,0 +1,22 @@
+
+// MARK: - Happy Number
+
+func isHappy(_ n: Int) -> Bool {
+    var N = n
+    var hash: [Int: Bool] = [:]
+    while N != 1 {
+        var tN = N
+        var sum = 0
+        while tN > 0 {
+            sum += (tN % 10) * (tN % 10)
+            tN /= 10
+        }
+        
+        if sum == 1 { return true }
+        else if hash[sum] != nil { return false }
+        hash[sum] = true
+        N = sum
+    }
+    return true
+}
+
