d34f6a9868e20689a0d4444fb03d074bb05c51ad
MinKyeongTae <kyeongtea92@gmail.com>
2020-08-04 19:56:32 +0900

    Solved Middle-of-the-Linked-List with more efficient way using swift

--- OtherAlgorithmStudy/LeetCode/30-Day-Challenge/Middle-of-the-Linked-List.swift
+++ OtherAlgorithmStudy/LeetCode/30-Day-Challenge/Middle-of-the-Linked-List.swift
@@ -1,7 +1,7 @@
 
 // MARK: - Middle of the Linked List
 
-// MARK: swift 문제풀이
+// MARK: O(N)[O(N)] swift 풀이방법
 /*
 /**
  * Definition for singly-linked list.
@@ -12,9 +12,24 @@
  *     public init(_ val: Int) { self.val = val; self.next = nil; }
  *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
  * }
- */
+ 
+class Solution {
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        var node = head
+        var result = head
+        var cnt = 0
+        while node?.next != nil {
+            result = result?.next
+            node = node?.next?.next
+        }
 
-
+        return result
+    }
+}
+*/
+ 
+/*
+// MARK: O(N)[O(2N)] swift 풀이방법
 class Solution {
     func middleNode(_ head: ListNode?) -> ListNode? {
         var node = head
