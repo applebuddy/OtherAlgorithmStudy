231d9c0ff24311e9500ef828863eee93ffe60c2b
MinKyeongTae <kyeongtea92@gmail.com>
2020-08-29 13:38:45 +0900

    Review Minimum-Path-Sum with DP using swift

--- OtherAlgorithmStudy/LeetCode/30-Day-Challenge/Minimum-Path-Sum.swift
+++ OtherAlgorithmStudy/LeetCode/30-Day-Challenge/Minimum-Path-Sum.swift
@@ -3,7 +3,31 @@
 
 // MARK: DP swift 문제풀이
 
+// - 200829) 복습 문제풀이
 class Solution {
+    func minPathSum(_ grid: [[Int]]) -> Int {
+        var DP = grid
+        for idx in grid.indices {
+            for jdx in grid[idx].indices {
+                if idx == 0, jdx == 0 { continue }
+                var one = Int.max, two = Int.max
+                if idx > 0 {
+                    one = DP[idx - 1][jdx]
+                }
+
+                if jdx > 0 {
+                    two = DP[idx][jdx - 1]
+                }
+
+                DP[idx][jdx] += one < two ? one : two
+            }
+        }
+
+        return DP.last!.last!
+    }
+}
+
+class Solution2 {
     func minPathSum(_ grid: [[Int]]) -> Int {
         var DP = grid
         for idx in grid.indices {
