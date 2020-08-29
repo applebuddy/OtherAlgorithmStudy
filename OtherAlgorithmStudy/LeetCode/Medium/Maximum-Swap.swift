91a2f40dc3d55383c49da866450df0cc558bcff2
MinKyeongTae <kyeongtea92@gmail.com>
2020-08-29 13:25:01 +0900

    Review Maximum-Swap using swift

--- OtherAlgorithmStudy/LeetCode/Medium/Maximum-Swap.swift
+++ OtherAlgorithmStudy/LeetCode/Medium/Maximum-Swap.swift
@@ -9,28 +9,54 @@ extension Character {
     }
 }
 
+// - 복습 문제풀이 (처음부터 각자리 수 Int타입으로 구해서 풀이)
 class Solution {
     func maximumSwap(_ num: Int) -> Int {
-        var arr = Array(String(num))
-        var last = [Int](repeating: 0, count: 10)
-        var idx = 0
-        arr.forEach {
-            last[$0.asciiInt()] = idx
-            idx += 1
+        var lastDic = [Int](repeating: 0, count: 10)
+        var numArray = Array(String(num)).map { Int($0.asciiValue! - 48) }
+        for idx in numArray.indices {
+            lastDic[numArray[idx]] = idx
         }
 
-        for (key, char) in arr.enumerated() {
-            var nowValue = 9
-            while nowValue > char.asciiInt() {
-                if last[nowValue] > key {
-                    arr.swapAt(key, last[nowValue])
-                    return arr.reduce(into: 0) { $0 = $0 * 10 + $1.asciiInt() }
+        for idx in numArray.indices {
+            var value = 9
+            while value > numArray[idx] {
+                if lastDic[value] > idx {
+                    numArray.swapAt(idx, lastDic[value])
+                    return numArray.reduce(into: 0) { $0 = $0 * 10 + $1 }
                 }
-
-                nowValue -= 1
+                value -= 1
             }
         }
 
         return num
     }
 }
+
+/*
+ class Solution2 {
+ func maximumSwap(_ num: Int) -> Int {
+     var arr = Array(String(num))
+     var last = [Int](repeating: 0, count: 10)
+     var idx = 0
+     arr.forEach {
+         last[$0.asciiInt()] = idx
+         idx += 1
+     }
+
+     for (key, char) in arr.enumerated() {
+         var nowValue = 9
+         while nowValue > char.asciiInt() {
+             if last[nowValue] > key {
+                 arr.swapAt(key, last[nowValue])
+                 return arr.reduce(into: 0) { $0 = $0 * 10 + $1.asciiInt() }
+             }
+
+             nowValue -= 1
+         }
+     }
+
+     return num
+ }
+ }
+ */
